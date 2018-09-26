712e12a8083e273ca0e7f41ef3e37e89f918ef95
Yash Talegaonkar <yash.talegaonkar37@gmail.com>
2018-09-26 09:53:54 +0530

    readme update  and pseudo file

--- MoveDir.py
+++ MoveDir.py
@@ -1,6 +1,3 @@
-# Python_Projects
-  1 .Create a python script to move files | from one directory to another. With using regex.
-
 # Create a search within directory with Parameter(Screen Shot, .png). 
 
 # The Working The directory will first search the name screen shot and then if it passes then it will 
